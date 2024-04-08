ed29cebef73290f4103e742d47567bb03e486ae4
Vladlen Schetinin <vvschetinin@gmail.com>
2024-04-08 19:50:58 +0300

    Стилизация отправки и результата

--- src/assets/js/components/formsend.js
+++ src/assets/js/components/formsend.js
@@ -1,2 +1,22 @@
 'use strict';
 
+const form = document.querySelector('#main-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', '/interface/post/send.php');
+  xhr.send(new FormData(form));
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      form.reset();
+      document.querySelector(".success span").innerHTML = xhr.responseText;
+      document.querySelector(".success").classList.add("success-mail");
+      setTimeout(function() {
+        document.querySelector(".success").classList.remove("success-mail");
+        document.querySelector(".success").style.display = "none";
+      }, 5000);
+    } else {
+      alert(xhr.statusText);
+    }
+  };
+});
